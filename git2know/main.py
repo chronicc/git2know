e333ee7481e8ad40158cc30da9569c381757c487
Thomas Steinert <hello@chroni.cc>
2024-04-01 23:28:07 +0200

    feat(ui): replace cli_ui with textual

--- git2know/main.py
+++ git2know/main.py
@@ -1,60 +1,111 @@
 #!/usr/bin/env python3
 
 from git import GitCmdObjectDB, Repo
-from git2know import HOME, INDEXDB, SYMBOL_CLEAN, SYMBOL_DIRTY, ui
+from git2know import HOME, INDEXDB, SYMBOL_CLEAN, SYMBOL_DIRTY
+from rich.text import Text
 from subprocess import check_call, check_output
+from textual.app import App, ComposeResult
+from textual.containers import ScrollableContainer
+from textual.reactive import reactive
+from textual.widgets import Header, Footer, DataTable
 import shutil
 
 
-def main():
-    ui.info("Updating index database")
-    check_call(
-        [
-            "updatedb",
-            "--add-prunenames",
-            ".cache .local",
-            "--database-root",
-            HOME,
-            "--output",
-            INDEXDB,
-            "--require-visibility",
-            "0",
-        ]
-    )
-    ui.info("Searching for git repositores")
-    common_args = ["-d", INDEXDB, "-r", "^.*/.git$"]
-    if shutil.which("mlocate"):
-        search_cmd = ["mlocate", "-q"]
-    elif shutil.which("locate"):
-        search_cmd = ["locate"]
-    else:
-        ui.error(
-            "No binary for reading the mlocate database found."
-            "Please install mlocate or locate."
-        )
-    repo_paths = check_output(search_cmd + common_args).decode().strip().split("\n")
-    ui.info("")
-    ui.info_section("Repositories")
-    for i, path in enumerate(repo_paths):
-        repo = Repo(path, odbt=GitCmdObjectDB)
+class Repository:
+    """A widget displaying information about a repository."""
 
-        if repo.is_dirty():
+    def __init__(self, path: str) -> None:
+        self.repo = Repo(path, odbt=GitCmdObjectDB)
+        super().__init__()
+
+    def __repr__(self) -> str:
+        if self.repo.is_dirty():
             status = SYMBOL_DIRTY
         else:
             status = SYMBOL_CLEAN
-
-        if repo.head.is_detached:
-            branch = "detached"
+        if self.repo.head.is_detached:
+            branch = "~detached~"
         else:
-            branch = repo.active_branch
+            branch = self.repo.active_branch
+        return (status, self.repo.working_dir, branch)
 
-        ui.info_count(
-            i,
-            len(repo_paths),
-            status,
-            f"[{branch}]",
-            repo.working_dir,
+
+class RepositoryList(DataTable):
+    """A widget displaying a list of repositories."""
+
+    table_header = ("Status", "Path", "Branch")
+    repositories = reactive([])
+
+    def on_mount(self) -> None:
+        self.cursor_type = "row"
+        self.zebra_stripes = True
+        self.add_columns(*self.table_header)
+
+    def watch_repositories(self) -> None:
+        self.clear()
+        for i, repo in enumerate(self.repositories):
+            label = Text(str(i + 1))
+            self.add_row(*repo.__repr__(), label=label)
+        self.focus()
+
+
+class Git2KnowApp(App):
+    """The main application."""
+
+    BINDINGS = [
+        ("d", "toggle_dark", "Toggle dark mode"),
+        ("ctrl+u", "update_repositories", "Update repositories"),
+    ]
+
+    TITLE = "git2know"
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield ScrollableContainer(RepositoryList())
+
+    def toggle_dark(self) -> None:
+        self.dark = not self.dark
+
+    def on_mount(self) -> None:
+        self.action_update_repositories()
+
+    def action_update_repositories(self) -> None:
+        print("Running action_update_repositories")
+        check_call(
+            [
+                "updatedb",
+                "--add-prunenames",
+                f".cache .local .pulumi .pyenv",
+                "--database-root",
+                HOME,
+                "--output",
+                INDEXDB,
+                "--require-visibility",
+                "0",
+            ]
         )
+        common_args = ["-d", INDEXDB, "-r", "^.*/.git$"]
+        if shutil.which("mlocate"):
+            search_cmd = ["mlocate", "-q"]
+        elif shutil.which("locate"):
+            search_cmd = ["locate"]
+        else:
+            raise Exception(
+                "No binary for reading the mlocate database found."
+                "Please install mlocate or locate."
+            )
+        repo_paths = check_output(search_cmd + common_args).decode().strip().split("\n")
+        repos = []
+        for _, path in enumerate(repo_paths):
+            repos.append(Repository(path))
+        self.query_one(RepositoryList).repositories = repos
+        print("Finished running action_update_repositories")
+
+
+def main():
+    app = Git2KnowApp()
+    app.run()
 
 
 if __name__ == "__main__":
